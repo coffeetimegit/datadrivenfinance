4d911cc69a64f6291d8e1631e60be988aeb05302
Yutaka Obi <obiyutak@gmail.com>
2020-06-15 10:27:49 +0900

    Integrated DRY codes.

--- app/src/main/java/view/BondValuationFragment.kt
+++ app/src/main/java/view/BondValuationFragment.kt
@@ -43,14 +43,11 @@ class BondValuationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         lateinit var option: Spinner
         lateinit var result: TextView
 
         option = view!!.findViewById(R.id.selectBond)
         result = view!!.findViewById(R.id.bondRes)
-
-        //var isins = mutableSetOf<String>()
 
         if (isins.isEmpty()) {
             var bondList = initBondList()
@@ -75,113 +72,56 @@ class BondValuationFragment : Fragment() {
                 }
                 isins.add(product)
             }
-
-
             bondDetails = bondList?.get(1).toString()
-
+        }
 
         option.adapter = ArrayAdapter(activity!!.applicationContext, android.R.layout.simple_list_item_1, isins.toList())
-
         option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 return null!!
             }
 
             override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
             ) {
                 result.text = isins.toList().get(position)
             }
         }
 
 
-            bondBtn.setOnClickListener {
+        bondBtn.setOnClickListener {
 
-                if (!bondRes.text.contains("Product Name")) {
+            if (!bondRes.text.contains("Product Name")) {
 
-                    if (bondRes.text == "Select JGB" || bondRes.text == "") {
-                        Toast.makeText(activity, "Error: Select a JGB from the dropdown!", Toast.LENGTH_SHORT).show()
+                if (bondRes.text == "Select JGB" || bondRes.text == "") {
+                    Toast.makeText(activity, "Error: Select a JGB from the dropdown!", Toast.LENGTH_SHORT).show()
+                } else {
+
+                    var bondCache = initFIV(bondDetails)
+                    var bondCacheImg = bondCache?.get(0).toString()
+                    var bondCacheTxt = bondCache?.get(1).toString()
+
+                    if (bondCacheImg != "error") {
+                        bondRes.text = bondCacheTxt
+                        val intent = Intent(view.context, Graph::class.java)
+                        intent.putExtra("res", bondCacheImg)
+                        startActivity(intent)
                     } else {
-
-                        var bondCache = initFIV(bondDetails)
-
-                        var bondCacheImg = bondCache?.get(0).toString()
-                        var bondCacheTxt = bondCache?.get(1).toString()
-
-                        if (bondCacheImg != "error") {
-                            bondRes.text = bondCacheTxt
-
-                            val intent = Intent(view.context, Graph::class.java)
-                            intent.putExtra("res", bondCacheImg)
-                            startActivity(intent)
-                        }
-
-                        if (bondCacheImg == "error") {
-                            Toast.makeText(activity, bondCacheTxt, Toast.LENGTH_LONG).show()
-                        }
+                        Toast.makeText(activity, bondCacheTxt, Toast.LENGTH_LONG).show()
                     }
                 }
             }
-        } else {
-
-            var bondDetails = bondDetails
-
-
-            option.adapter = ArrayAdapter(activity!!.applicationContext, android.R.layout.simple_list_item_1, isins.toList())
-
-            option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    return null!!
-                }
-
-                override fun onItemSelected(
-                        parent: AdapterView<*>?,
-                        view: View?,
-                        position: Int,
-                        id: Long
-                ) {
-                    result.text = isins.toList().get(position)
-                }
-            }
-
-
-            bondBtn.setOnClickListener {
-
-                if (!bondRes.text.contains("Product Name")) {
-
-                    if (bondRes.text == "Select JGB" || bondRes.text == "") {
-                        Toast.makeText(activity, "Error: Select a JGB from the dropdown!", Toast.LENGTH_SHORT).show()
-                    } else {
-
-                        var bondCache = initFIV(bondDetails)
-
-                        var bondCacheImg = bondCache?.get(0).toString()
-                        var bondCacheTxt = bondCache?.get(1).toString()
-
-                        if (bondCacheImg != "error") {
-                            bondRes.text = bondCacheTxt
-
-                            val intent = Intent(view.context, Graph::class.java)
-                            intent.putExtra("res", bondCacheImg)
-                            startActivity(intent)
-                        }
-
-                        if (bondCacheImg == "error") {
-                            Toast.makeText(activity, bondCacheTxt, Toast.LENGTH_LONG).show()
-                        }
-                    }
-                }
-            }
-
         }
 
         bondClear.setOnClickListener {
             bondRes.text = ""
         }
     }
+
 
     private fun initFIV(bondDetails: String?): MutableList<PyObject>? {
         val python = Python.getInstance()
@@ -196,8 +136,8 @@ class BondValuationFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bondvaluation, container, false)
@@ -215,11 +155,11 @@ class BondValuationFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BondValuationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                BondValuationFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
